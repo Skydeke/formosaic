1af90b9ba66dfaf2f0147bbb1882c64b16945ba3
David Metzler <david-metzler@web.de>
2026-03-21 19:47:52 +0100

    Remove stuff that should have never been created

--- engine/src/opengl/shaders/mod.rs
+++ engine/src/opengl/shaders/mod.rs
@@ -3,11 +3,9 @@ pub mod lighting_pass;
 pub mod render_state;
 pub mod shader;
 pub mod shader_program;
-pub mod simple_program;
 pub mod uniform;
 
 pub use render_state::RenderState;
 pub use shader::Shader;
 pub use shader_program::ShaderProgram;
-pub use simple_program::SimpleProgram;
 pub use uniform::{Uniform, UniformFloat, UniformInt, UniformMatrix4, UniformVec2, UniformVec3};
